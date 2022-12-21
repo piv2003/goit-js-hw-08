51cf6d7bda031efebc16295ad21140df386a7eab
Ihor Poliakov <piv2003@gmail.com>
2022-12-21 23:07:43 +0200

    adding to Task-1 & import Task-2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,4 +3,38 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
-console.log(SimpleLightbox);
+// console.log(SimpleLightbox);
+
+const galleryImgRef = document.querySelector('.gallery');
+const imgMarkup = createItemsMarkup(galleryItems);
+galleryImgRef.insertAdjacentHTML("beforeend", imgMarkup);
+
+function createItemsMarkup() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img
+          class="gallery__image"
+          src="${preview}"          
+          alt="${description}"
+        />
+      </a>
+      `;
+    })
+    .join("");
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+    scrollZoom: false,
+    captionDelay: 250,
+    captionsData: 'alt',
+    doubleTapZoom: 1,
+  });
+
+  galleryImgRef.addEventListener('click', event => {
+    event.preventDefault();
+    if (!event.target.classList.contains('gallery__image')) {
+      return;
+    }
+  });
